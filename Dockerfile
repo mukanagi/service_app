111a4083669a2426127a02d43470f41a93924c63
Artem <26rus5g@gmail.com>
2023-01-08 20:46:18 +0300

    add-models

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM python:3.9-alpine3.16
 
-
 COPY requirements.txt /temp/requirements.txt
 COPY service /service
 WORKDIR /service
--- service/clients/admin.py
+++ service/clients/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from clients.models import Client
+
+admin.site.register(Client)
 
 # Register your models here.
